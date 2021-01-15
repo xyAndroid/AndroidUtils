10f0dd080e4996d458804d7090978e758dc44e63
XieYan <xieyanlily@gmail.com>
2021-01-15 11:13:08 +0800

    增加json工具类

---
+++ lib_android_utils/src/main/java/com/xy/lib/utils/json/ImpParameterizedType.java
@@ -0,0 +1,42 @@
+package com.xy.lib.utils.json;
+
+
+import androidx.annotation.NonNull;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * create by chl on 2019/12/12
+ * description : 参考 https://www.jianshu.com/p/d62c2be60617
+ */
+class ImpParameterizedType implements ParameterizedType {
+
+    private Class raw;
+    private Type[] args;
+    private Type owner;
+
+    public ImpParameterizedType(Class raw, Type[] args, Type owner) {
+        this.raw = raw;
+        this.args = args != null ? args : new Type[0];
+        this.owner = owner;
+    }
+
+
+    @NonNull
+    @Override
+    public Type[] getActualTypeArguments() {
+        return args;
+    }
+
+    @NonNull
+    @Override
+    public Type getRawType() {
+        return raw;
+    }
+
+    @Override
+    public Type getOwnerType() {
+        return owner;
+    }
+}
